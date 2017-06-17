2c00caf405a747dc472a682fd30e9820f90cdfc7
James Mbabazi <jmbabazi@pih.org>
2017-06-17 12:25:54 -0700

    v1

--- patient_attributes.sql
+++ patient_attributes.sql
@@ -1,1 +1,8 @@
-SELECT pi.patient_id AS 'ID', pi.identifier AS 'EMR ID', p.gender AS 'Gender' from patient_identifier pi join person p on pi.patient_id=p.person_id;
+SELECT 
+    pi.patient_id AS 'ID',
+    pi.identifier AS 'EMR ID',
+    p.gender AS 'Gender'
+FROM
+    patient_identifier pi
+        JOIN
+    person p ON pi.patient_id = p.person_id;
