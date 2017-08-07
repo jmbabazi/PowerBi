aceeb05352f05d5e73e879f99b11dc92c7397b53
James Mbabazi <jmbabazi@pih.org>
2017-08-07 13:01:03 -0400

    Create p_age.sql

---
+++ p_age.sql
@@ -0,0 +1,9 @@
+SELECT 
+    pi.patient_id,
+    p.gender,
+    p.birthdate,
+    FLOOR(DATEDIFF(CURDATE(), p.birthdate) / 365) AS age
+FROM
+    person p
+        INNER JOIN
+    patient_identifier pi ON pi.patient_id = p.person_id;
