ad9f5d71966290e82871da58041ded850cd4de34
James Mbabazi <jmbabazi@pih.org>
2017-08-07 13:33:39 -0400

    Patient's age with age range.
    Age (0-10, 10-20, 20-30, 30-40, 40-50, 50-60, 60-70, >70)

--- p_age.sql
+++ p_age.sql
@@ -2,7 +2,10 @@ SELECT
     pi.patient_id,
     p.gender,
     p.birthdate,
-    FLOOR(DATEDIFF(CURDATE(), p.birthdate) / 365) AS age
+    FLOOR(DATEDIFF(CURDATE(), p.birthdate) / 365) AS age,
+    CONCAT(10 * FLOOR(FLOOR(DATEDIFF(CURDATE(), p.birthdate) / 365) / 10),
+            '-',
+            10 * FLOOR(FLOOR(DATEDIFF(CURDATE(), p.birthdate) / 365) / 10) + 10) AS age_range
 FROM
     person p
         INNER JOIN
